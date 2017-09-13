9a7851fe4c42ff9b074a981cc8c3633ec16bd971
[王梦涛] <[568283034@qq.com]>
2017-09-13 18:28:17 +0800

    update template

--- dist/template/starter.vue
+++ dist/template/starter.vue
@@ -1,8 +1,75 @@
 <template>
-  
+
 </template>
 <script>
-  
+  export default {
+    filters: [],
+    mixins: [],
+    directives: {
+
+    },
+    props: {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    computed: {
+
+    },
+    watch: {
+
+    },
+    methods: {
+
+    },
+    components: {
+
+    },
+    beforeCreate () {
+
+    },
+    created () {
+
+    },
+    beforeMount () {
+
+    },
+    mounted () {
+
+    },
+    beforeUpdate () {
+
+    },
+    updated () {
+
+    },
+    activated () {
+
+    },
+    deactivated () {
+
+    },
+    beforeDestroy () {
+
+    },
+    destroyed () {
+
+    },
+    beforeRouteEnter (to, from, next) {
+      next(vm => {
+
+      })
+    },
+    beforeRouteUpdate (to, from, next) {
+      next()
+    },
+    beforeRouteLeave (to, from, next) {
+      next()
+    }
+  }
 </script>
 <style lang="less">
 
